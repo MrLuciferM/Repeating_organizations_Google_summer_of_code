1d6df58fe53d7048996975fb37f5fff3e1aacc5b
Kunal <pps.kunal.7@gmail.com>
2020-03-06 20:44:26 +0530

    first

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Repeating_organizations_Google_summer_of_code
+ searches repeaating organizations in google summer of code using web scrapping
+ using beautifulsoup
---
+++ test.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+import numpy as np
+
+
+
+
+
+#to combine the string replacing the white spaces 
+def combine(year,soc):
+    myurl = soc[0]+year+soc[1]
+    return myurl
+
+def see(years,soc):
+    orgs=[]
+    for year in years:
+        myurl= combine(year,soc)
+        uClient = uReq(myurl)
+        page_html = uClient.read()
+        uClient.close()
+        page_soup = soup(page_html,"html.parser")
+        headings = page_soup.findAll("h4",{"class":"organization-card__name"})
+        # print(f'{year}\n\n')
+        
+        for heading in headings:
+            # print(soup.prettify(heading))
+            orgs.append(heading.text)
+            # print(orgs)
+    return orgs
+    # print(orgs)
+
+def consistent(orgs):
+    # uniqueOrgs = set(orgs)
+    finalres=dict()
+    orgsRepeat = {org:orgs.count(org) for org in orgs}
+    
+    for org,times in orgsRepeat.items():
+        if times>1:
+            finalres[org]=times
+    for key,val in finalres.items():
+        print(f"{key}:{val}")
+
+
+if __name__ == "__main__":
+    soc = ['https://summerofcode.withgoogle.com/archive/','/organizations']
+    years = ['2017','2018','2019']
+    orgs=see(years,soc)
+    # print(orgs)
+    consistent(orgs)
